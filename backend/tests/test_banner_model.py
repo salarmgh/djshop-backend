6bec8af557265bb6c20b91704d6de05af8d9010d
Salar Moghaddam <salarmgh@gmail.com>
2019-10-08 01:12:30 +0330

    Finish all models tests

--- backend/tests/test_banner_model.py
+++ backend/tests/test_banner_model.py
@@ -4,35 +4,47 @@ import os
 import re
 from django.conf import settings
 from ..models import Banner
-from .helpers import generate_random_string, generate_random_number
+from .helpers import generate_random_string, generate_random_number, check_file_exists_on_model, check_filename_is_same_on_model 
 import shutil
 
     
 class BannerTests(TestCase):
     def setUp(self):
-        self.image_name = "image.jpg"
+        self.image_name = generate_random_string(3, 5) + '.jpg'
         self.title = generate_random_string(15, 50) 
         self.description = generate_random_string(15, 50) 
         self.url = generate_random_string(15, 50) 
         self.image = SimpleUploadedFile(name=self.image_name, content=open('backend/tests/assets/placeholder.jpg', 'rb').read(), content_type='image/jpeg')
+        self.banner = Banner.objects.create(title=self.title, description=self.description, url=self.url, image=self.image)
 
 
     def test_can_create_banner(self):
         """
         Ensure we can create a new banner object.
         """
-        banner = Banner.objects.create(title=self.title, description=self.description, url=self.url, image=self.image)
 
-        self.assertEqual(banner.title, self.title)
-        self.assertEqual(banner.description, self.description)
-        self.assertEqual(banner.url, self.url)
+        self.assertEqual(self.banner.title, self.title)
+        self.assertEqual(self.banner.description, self.description)
+        self.assertEqual(self.banner.url, self.url)
 
-        filename, file_extension = os.path.splitext(self.image_name)
-        same_name = re.match(settings.BANNER_IMAGES_DIR + filename + ".*" + file_extension, banner.image.name)
-        self.assertTrue(same_name)
+    def test_is_filename_same_on_model(self):
+        """
+        Ensure filename is same on the model as we want
+        """
+        self.assertTrue(check_filename_is_same_on_model(self.banner, settings.BANNER_IMAGES_DIR, self.image_name, settings.MEDIA_DIR))
+    
+    def test_is_file_exists_on_model(self):
+        """
+        Ensure file is present on the file system
+        """
+        self.assertTrue(check_file_exists_on_model(self.banner, settings.BANNER_IMAGES_DIR, self.image_name, settings.MEDIA_DIR))
 
-        file_exists = os.path.isfile(settings.MEDIA_DIR + banner.image.name)
-        self.assertTrue(file_exists)
+    def test_can_delete_image(self):
+        """
+        Ensure we can delete a image object with the file.
+        """
+        self.banner.delete()
+        self.assertFalse(check_file_exists_on_model(self.banner, settings.BANNER_IMAGES_DIR, self.image_name, settings.MEDIA_DIR))
 
     def tearDown(self):
         shutil.rmtree(settings.MEDIA_DIR)
--- backend/tests/test_carousel_model.py
+++ backend/tests/test_carousel_model.py
@@ -4,35 +4,47 @@ import os
 import re
 from django.conf import settings
 from ..models import Carousel
-from .helpers import generate_random_string, generate_random_number
+from .helpers import generate_random_string, generate_random_number, check_file_exists_on_model, check_filename_is_same_on_model 
 import shutil
 
     
 class CarouselTests(TestCase):
     def setUp(self):
-        self.image_name = "image.jpg"
+        self.image_name = generate_random_string(3, 5) + '.jpg'
         self.title = generate_random_string(15, 50) 
         self.description = generate_random_string(15, 50) 
         self.url = generate_random_string(15, 50) 
         self.image = SimpleUploadedFile(name=self.image_name, content=open('backend/tests/assets/placeholder.jpg', 'rb').read(), content_type='image/jpeg')
+        self.carousel = Carousel.objects.create(title=self.title, description=self.description, url=self.url, image=self.image)
 
 
     def test_can_create_carousel(self):
         """
         Ensure we can create a new carousel object.
         """
-        carousel = Carousel.objects.create(title=self.title, description=self.description, url=self.url, image=self.image)
 
-        self.assertEqual(carousel.title, self.title)
-        self.assertEqual(carousel.description, self.description)
-        self.assertEqual(carousel.url, self.url)
+        self.assertEqual(self.carousel.title, self.title)
+        self.assertEqual(self.carousel.description, self.description)
+        self.assertEqual(self.carousel.url, self.url)
 
-        filename, file_extension = os.path.splitext(self.image_name)
-        same_name = re.match(settings.CAROUSEL_IMAGES_DIR + filename + ".*" + file_extension, carousel.image.name)
-        self.assertTrue(same_name)
+    def test_is_filename_same_on_model(self):
+        """
+        Ensure filename is same on the model as we want
+        """
+        self.assertTrue(check_filename_is_same_on_model(self.carousel, settings.CAROUSEL_IMAGES_DIR, self.image_name, settings.MEDIA_DIR))
+    
+    def test_is_file_exists_on_model(self):
+        """
+        Ensure file is present on the file system
+        """
+        self.assertTrue(check_file_exists_on_model(self.carousel, settings.CAROUSEL_IMAGES_DIR, self.image_name, settings.MEDIA_DIR))
 
-        file_exists = os.path.isfile(settings.MEDIA_DIR + carousel.image.name)
-        self.assertTrue(file_exists)
+    def test_can_delete_image(self):
+        """
+        Ensure we can delete a image object with the file.
+        """
+        self.carousel.delete()
+        self.assertFalse(check_file_exists_on_model(self.carousel, settings.CAROUSEL_IMAGES_DIR, self.image_name, settings.MEDIA_DIR))
 
     def tearDown(self):
         shutil.rmtree(settings.MEDIA_DIR)
