8ee8975efb0129f0c645b3e9bb70d1ee7bacfe52
Salar Moghaddam <salarmgh@gmail.com>
2019-11-17 18:36:39 +0330

    Add image serializer for admin

--- backend/admin.py
+++ backend/admin.py
@@ -11,23 +11,17 @@ from django.contrib.admin.options import flatten_fieldsets
 
 class AdminSite(AdminSite):
     site_header = 'Anemone'
+
     def get_urls(self):
         from django.urls import path
         urls = super().get_urls()
 
         return urls
 
-    
+
 class AddressInline(admin.StackedInline):
     model = Address
     extra = 1
-    
-
-# class CategoryProductInline(admin.StackedInline):
-#     model = Category.products.through
-#     extra = 3
-#     verbose_name = "Products"
-#     verbose_name_plural = "Products"
 
 
 class CategoryAttributeInline(admin.StackedInline):
@@ -35,25 +29,19 @@ class CategoryAttributeInline(admin.StackedInline):
     verbose_name = "Attributes"
     verbose_name_plural = "Attributes"
     extra = 3
-    
 
 
 class ImageInline(admin.StackedInline):
     model = Image
     extra = 3
 
+
 class ProductVariantsForm(ModelForm):
-    
     class Meta:
         model = Variant
         fields = ('name', 'attribute_values', 'product', 'price', 'images',)
-        
-    def __init__(self, *args, **kwargs):
 
-        super().__init__(*args, **kwargs)
-        self.fields['test'] = CharField(label='test')
 
-        
 class ProductVariantsInline(admin.StackedInline):
     model = Variant
     form = ProductVariantsForm
@@ -62,28 +50,6 @@ class ProductVariantsInline(admin.StackedInline):
     verbose_name_plural = "Variants"
 
 
-    
-    # def get_fieldsets(self, request, obj=None):
-    #     # if self.declared_fieldsets:
-    #     #     return self.declared_fieldsets
-
-    #     form = self.get_formset(request, obj)(instance=obj)
-    #     return [(None, {'fields': form.fields.keys()})]
-
-    # def get_queryset(self, request):
-    #     qs = super().get_queryset(request)
-    #     category_attributes = qs.all()[0].product.category.attributes.all()
-    #     attributes = Attribute.objects.filter(pk__in=category_attributes)
-    #     return attributes
-    
-    
-# class ProductCategoryInline(admin.StackedInline):
-#     model = Product.categories.through
-#     extra = 1
-#     verbose_name = "Categories"
-#     verbose_name_plural = "Categories"
-
-    
 class AttributeValueInline(admin.StackedInline):
     model = AttributeValue
     extra = 3
@@ -93,17 +59,18 @@ class AttributeValueInline(admin.StackedInline):
 
 class UserForm(ModelForm):
     password = CharField(widget=PasswordInput())
-    
+
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'number', 'groups', 'password', 'is_active', 'is_superuser',)
+        fields = ('username', 'first_name', 'last_name', 'email',
+                  'number', 'groups', 'password', 'is_active', 'is_superuser',)
 
-    
+
 class UserAdmin(admin.ModelAdmin):
     form = UserForm
     inlines = [AddressInline]
-    readonly_fields=('last_login', 'date_joined',)
-    
+    readonly_fields = ('last_login', 'date_joined',)
+
     def save_model(self, request, obj, form, change):
         obj.set_password(obj.password)
         super().save_model(request, obj, form, change)
@@ -111,15 +78,15 @@ class UserAdmin(admin.ModelAdmin):
 
 class ProductForm(ModelForm):
     url = CharField()
-    
+
     class Meta:
         model = Product
         fields = ('title', 'description', 'featured', 'category',)
 
-        
+
 class CategoryForm(ModelForm):
     url = CharField()
-    
+
     class Meta:
         model = Category
         fields = ('name', 'image',)
@@ -129,11 +96,10 @@ class ProductAdmin(admin.ModelAdmin):
     def url(self, obj):
         return mark_safe('<a href="/products/{}/">{}</a>'.format(obj.slug, obj.title))
 
-
     inlines = [ProductVariantsInline]
     form = ProductForm
-    readonly_fields=('created_at','url',)
-    
+    readonly_fields = ('created_at', 'url',)
+
 
 class AttributeAdmin(admin.ModelAdmin):
     inlines = [AttributeValueInline]
@@ -142,13 +108,14 @@ class AttributeAdmin(admin.ModelAdmin):
 class CategoryAdmin(admin.ModelAdmin):
     def url(self, obj):
         return mark_safe('<a href="/categories/{}/">{}</a>'.format(obj.slug, obj.name))
-    
+
     inlines = [CategoryAttributeInline]
     form = CategoryForm
-    readonly_fields=('url',)
+    readonly_fields = ('url',)
 
     class Meta:
         model = Category
+
 
 admin_site = AdminSite()
 
@@ -164,4 +131,5 @@ admin_site.register(LandingBanner)
 admin_site.register(Order)
 admin_site.register(Cart)
 admin_site.register(Variant)
+admin_site.register(Image)
 #admin_site.register(Category, CustomAdmin)
