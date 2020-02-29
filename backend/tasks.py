90eaa9a25d4335bb0d64342540a06b31451ace15
Salar Moghaddam <salarmgh@gmail.com>
2020-02-29 19:23:40 +0330

    Add product indexing

--- backend/tasks.py
+++ backend/tasks.py
@@ -8,14 +8,29 @@ from anemone.elasticsearch import connections
 
 @shared_task(bind=True, max_retries=600)
 def variant_model_indexer(self, model):
-    variant = VariantDocument(meta={'id': model["id"]}, name=model["name"], price=model["price"], product=model["product"], images=model["images"], attributes=model["attributes"], category=model["category"])
+    variant = VariantDocument(meta={'id': model["id"]}, name=model["name"], price=model["price"],
+                              product=model["product"], images=model["images"], attributes=model["attributes"], category=model["category"])
     try:
         variant.save()
     except Exception as e:
         self.retry(countdown=30, exc=e)
     return model
 
+
 @shared_task(bind=True, max_retries=600)
 def variant_bulk_indexer(self):
     from .models import Variant
-    bulk(client=connections.get_connection(), actions=(b.indexing() for b in Variant.objects.all().iterator()))
+    bulk(client=connections.get_connection(), actions=(b.indexing()
+                                                       for b in Variant.objects.all().iterator()))
+
+
+@shared_task(bind=True, max_retries=600)
+def product_model_indexer(self, variants):
+    print(variants)
+    for variant in variants.iterator():
+        variant.indexing()
+        try:
+            variant.save()
+        except Exception as e:
+            self.retry(countdown=30, exc=e)
+    return variant
